2c12f9eb2c3fff2bf6413443774c617db780e115
Nabeel Ali <97429829+NabeelWasTaken@users.noreply.github.com>
2026-07-21 04:11:03 +0400

    Add: two-integer-sum - submission-32

---
+++ Data Structures & Algorithms/two-integer-sum/submission-32.py
@@ -0,0 +1,22 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+
+        exists = {}
+
+        for i in range(len(nums)):
+            diff = target - nums[i]
+            if diff in exists:
+                return [exists.get(diff), i]
+            
+            exists[nums[i]] = i
+        
+        return -1
+                
+    
+
+
+            
+        
+                    
+
+        
